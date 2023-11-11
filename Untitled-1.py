a208f54084e27fb56283ad29beca0b7a239a568c
Harambutt <147921745+Harambutt@users.noreply.github.com>
2023-11-12 00:52:20 +0500

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,8 @@
+faculty = ["uzair","ali","usman","saif"]
+cofounder = []
+cofounder.insert(0,"uzair")
+cofounder.insert(2,"usman")
+faculty.remove("uzair")
+faculty.remove("saif")
+print("faculty")
+print("cofounder")
