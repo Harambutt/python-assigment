2ce408a1d2157eb7545a582f02438e3db86ba89c
Harambutt <147921745+Harambutt@users.noreply.github.com>
2023-11-12 00:58:18 +0500

    Delete Untitled-2.py

--- Untitled-2.py
+++
@@ -1,8 +0,0 @@
-faculty = ["usman","saif","ali"]
-cofounder =[]
-cofounder.append("saif")
-cofounder.append("ali")
-print("cofounder")
-faculty.remove("saif")
-faculty.append("ali")
-print("faculty")
